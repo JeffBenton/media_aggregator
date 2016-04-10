222b4b6ae00d350db77f9135bf477121f8e03cd2
Jeff Benton <Jeff@frogrock.net>
2016-04-10 16:03:54 -0700

    Removed MySql to add to different layer

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ app.use(express.static(path.join(__dirname, './static')));
 app.set('views', path.join(__dirname, './static'));
 app.set('view engine', 'ejs');
 
-require('./config/mysql.js');
 require('./config/routes.js')(app);
 
 app.listen(8000);
